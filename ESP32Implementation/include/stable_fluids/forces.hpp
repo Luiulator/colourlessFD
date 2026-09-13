1414b36cc7bed94f06f5c6b818a07ee2119331c7
Luis <111397112+Luiulator@users.noreply.github.com>
2026-09-13 18:50:16 +0200

    feat: add buoyancy force implementation for ESP32 implementation

---
+++ ESP32Implementation/include/stable_fluids/forces.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "types.hpp"
+
+namespace stable_fluids {
+template <size_t NX, size_t NY>
+void add_buoyancy(FluidGrid2D<NX, NY> &grid, float gx = 0.0f,
+                  float gy = -9.81f) {
+  float dt = grid.dt;
+
+  for (size_t i = 1; i <= NX; i++) {
+    for (size_t j = 1; j <= NY; j++) {
+
+      float d = grid.density[i][j];
+      grid.u[i][j] += d * gx * dt;
+      grid.v[i][j] += d * gy * dt;
+    }
+  }
+}
+
+}
