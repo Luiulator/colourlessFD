e28266c348ef541a42f4e3854f66dd1a73e03c8b
Luis <111397112+Luiulator@users.noreply.github.com>
2026-09-14 08:17:21 +0200

    refactor: pass grid by reference and fix argument syntax in step function

--- ESP32Implementation/include/stable_fluids/StableFluids.hpp
+++ ESP32Implementation/include/stable_fluids/StableFluids.hpp
@@ -1,22 +1,21 @@
 #pragma once
 
 #include "advection.hpp"
-#include "boundary.hpp"
 #include "diffusion.hpp"
-#include "forces.hpp"
 #include "projection.hpp"
 #include "types.hpp"
+#include <cstring>
 
 namespace stable_fluids {
 template <size_t NX, size_t NY>
-void step(FluidGrid2D<NX, NY> grid, int iter = 20) {
-  FluidGrid2D<NX, NY> &params = grid.params;
+void step(FluidGrid2D<NX, NY>& grid, int iter = 20) {
+  FluidParams<NX, NY>& params = grid.params;
 
   if (params.visc > 0.0f) {
     std::memcpy(grid.u_prev, grid.u, sizeof(grid.u));
     std::memcpy(grid.v_prev, grid.v, sizeof(grid.v));
-    diffuse(grid.u, grid.u_prev, params.visc, 1, params, iter = iter);
-    diffuse(grid.v, grid.v_prev, params.visc, 2, params, iter = iter);
+    diffuse(grid.u, grid.u_prev, params.visc, 1, params, iter);
+    diffuse(grid.v, grid.v_prev, params.visc, 2, params, iter);
   }
 
   project(grid.u, grid.v, grid.p, grid.div, params, iter = iter);
@@ -24,10 +23,10 @@ void step(FluidGrid2D<NX, NY> grid, int iter = 20) {
   std::memcpy(grid.u_prev, grid.u, sizeof(grid.u));
   std::memcpy(grid.v_prev, grid.v, sizeof(grid.v));
 
-  advect(grid.u, grid.u_prev, grid.u_perv, grid.v_prev, 1, params);
+  advect(grid.u, grid.u_prev, grid.u_prev, grid.v_prev, 1, params);
   advect(grid.v, grid.v_prev, grid.u_prev, grid.v_prev, 2, params);
 
-  project(grid.u, grid.v, grid.p, grid.div, params, iter = iter);
+  project(grid.u, grid.v, grid.p, grid.div, params, iter);
 
   if (params.diff > 0.0f) {
     std::memcpy(grid.density_prev, grid.density, sizeof(grid.density));
